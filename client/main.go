3e8b34924994b48eb8b8b85aa5b1158a1dadbfb1
fcanbekli <fatih.canbekli@outlook.com>
2023-11-18 18:50:58 +0300

    Add methods

--- client/main.go
+++ client/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "mrpc"
+import (
+	"fmt"
+	"mrpc"
+)
 
 func main() {
 	mrpc.SayHello()
+	mrpc.SayMyName()
+	mrpc.SayTest()
+
+	rpc := mrpc.DialServer("x", "8080")
+	
+	// Access and print the fields of the struct
+	fmt.Println("First Name:", rpc.Sum(2, 4))
 }
--- demo/math_mrpc.go
+++ demo/math_mrpc.go
@@ -6,3 +6,36 @@ import "fmt"
 func SayHello() {
 	fmt.Println("DATA")
 }
+
+func SayMyName() {
+	fmt.Println("MyName")
+}
+
+func SayTest() {
+	fmt.Println("Test")
+}
+
+// Client -------------------- Client
+type RpcService struct{}
+
+func (rpc *RpcService) Sum(a int, b int) int {
+	return sum_rpc(a, b)
+	// Send method name and data via tcp connection
+	// Wait until have response
+}
+
+func DialServer(ip string, port string) RpcService {
+	return RpcService{}
+}
+
+// Client -------------------- Client
+
+// Server -------------------- Server
+func StartServer(port string) {
+	//Start TCP Socket
+}
+
+func sum_rpc(a int, b int) int {
+	return a + b
+	// Real calculation here
+}
